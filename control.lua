159fdd4c3d52d8edd892ef0b63cc5d27e800c4e0
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2015-08-31 20:36:28 -0700

    version 1.1.0 alpha (working version)

--- control.lua
+++ control.lua
@@ -6,6 +6,47 @@ require "gui"
 --[[    Inspiration from BiG_MEECH    ]]--
 -- GPLv2 --
 
+local int32_max = 2147483647
+local debug = true
+
+function serialize(val, depth)
+    depth = depth or 0
+
+    local tmp = string.rep(" ", depth)
+    local ty = type(val)
+
+    if ty == "table" then
+        tmp = tmp .. "{" .. "\n"
+
+        for k, v in pairs(val) do
+            tmp = tmp .. string.format("%q", k) .. " : " .. serialize(v, depth + 1) .. "," .. "\n"
+        end
+
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif ty == "number" then
+        tmp = tmp .. tostring(val)
+    elseif ty == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif ty == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[?:" .. type(val) .. "]\""
+    end
+
+    return tmp
+end
+
+function log(string, force)
+  if not debug and not force then return end
+  --for i, player in ipairs(game.players) do
+  --  player.print(string)
+  --end
+  local player = game.get_player("Lugnuts")
+  player.print(string)
+end
+
+--[[ real stuff below ]]--
+
 game.on_init(function() init() end)
 game.on_load(function() init() end)
 
@@ -70,32 +111,80 @@ end)
 game.on_event(defines.events.on_entity_died, function(event)
   local player = event.entity
   if player.type ~= "player" then return end
+
+  function shrinkStack(count)
+    local newCount = 0
+    for j=1,count do
+      newCount = newCount + (math.random() < global.settings.percentageRetained and 1 or 0)
+    end
+    return newCount
+  end
   
   local transfer_stack --function to deal with stacks, based on mode
-  if global.settings.mode == 1 then -- chest
+  if global.settings.mode == 1 then --[[ CHEST/GRAVESTONE ]]--
     local pos = player.surface.find_non_colliding_position(
-      "gravestone", player.position, 8, 1)
-    if not pos then return end -- shouldn't happen
+      "gravestone", player.position, int32_max, 1) --should always spawn
+    if not pos then -- gg no re
+      log("Unable to spawn gravestone within " .. int32_max .. " tiles.", true)
+      return
+    end
     
     local grave = player.surface.create_entity{
-      name="gravestone", position=pos, force=player.force}
+      name = "gravestone", position = pos, force = player.force }
+    log("Spawned gravestone at (" .. pos.x .. ", " .. pos.y .. ")")
 
     transfer_stack = (function(inv)
-      return (function(stack, i)
-        if i > #inv then return end
+      local i = 1
+      return (function(stack)
+        log("Grave: processing \"" .. stack.name .. "\"x" .. stack.count)
+        if not global.settings.saveInventories or not stack.has_grid then
+          log("Shrinking stack")
+          stack.count = shrinkStack(stack.count)
+        end
+        if i > #inv or not stack.valid_for_read then
+          log("Stack shrunk to zero, or gravestone full")
+          return
+        end
+        log("Setting slot " .. i .. " to \"" .. stack.name .. "\"x" .. stack.count .. " and incrementing")
         inv[i].set_stack(stack)
+        i = i + 1
       end)
     end)(grave.get_inventory(defines.inventory.chest))
-  else -- ground
-    transfer_stack = (function(stack, i)
-      player.surface.spill_item_stack(stack, player.position) --will this work with ItemStack's instead of SimpleItemStack's?
+
+  else --[[ DROP ON GROUND ]]--
+    transfer_stack = (function(stack)
+      log("Drop: processing \"" .. stack.name .. "\"x" .. stack.count)
+      log("Drop")
+
+      if stack.has_grid then --will also work with SimpleItemStacks (has_grid == nil)
+        log("Stack is modular {")
+        local equipments = stack.grid.take_all()
+        for name, count in pairs(equipments) do
+          log("Equipment: \"" .. name .. "\"x" .. count)
+          player.surface.spill_item_stack({ name = name, count = count }, player.position) --always spawn equipment
+        end
+        if global.settings.saveInventories then
+          log("} Not shrinking stack")
+          player.surface.spill_item_stack(stack, player.position)
+          return
+        end
+        log("}")
+      end
+
+      log("test")
+
+      stack.count = shrinkStack(stack.count)
+      if not stack.valid_for_read then
+        log("Stack shrunk to 0 (removed)")
+        return
+      end
+      log("Stack shrunk to " .. stack.count)
+      player.surface.spill_item_stack(stack, player.position)
     end)
   end
 
-  local count = 0
   if player.cursor_stack ~= nil and player.cursor_stack.valid_for_read then
-    count = count + 1
-    transfer_stack(player.cursor_stack, count)
+    transfer_stack(player.cursor_stack)
   end
   for i, id in ipairs{
       defines.inventory.player_guns,
@@ -108,8 +197,7 @@ game.on_event(defines.events.on_entity_died, function(event)
     local inv = player.get_inventory(id)
     for j = 1, #inv do
       if inv[j].valid_for_read then
-        count = count + 1
-        transfer_stack(inv[j], count)
+        transfer_stack(inv[j])
       end
     end
   end
